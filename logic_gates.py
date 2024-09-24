56bb55ae934f22573f3a9b9aadbd9ae9fcf48405
xosun <hello@daniellong.me>
2024-09-24 15:25:30 -0500

    Reformat code styles

--- logic_gates.py
+++ logic_gates.py
@@ -1,12 +1,15 @@
 def nand_gate(a, b):
     return not (a and b)
 
+
 def and_gate(a, b):
     ab_nand = nand_gate(a, b)
     return nand_gate(ab_nand, ab_nand)
 
+
 def not_gate(a):
     return nand_gate(a, a)
+
 
 def or_gate(a, b):
     aa_nand = nand_gate(a, a)
--- test_logic_gates.py
+++ test_logic_gates.py
@@ -1,57 +1,62 @@
 import unittest
 from logic_gates import nand_gate, and_gate, not_gate, or_gate
 
+
 class TestNandGate(unittest.TestCase):
     def test_nand_gate_all_false(self):
-        self.assertEqual(nand_gate(False, False), True)
+        self.assertEqual(nand_gate(0, 0), 1)
 
     def test_nand_gate_one_false(self):
-        self.assertEqual(nand_gate(False, True), True)
-        self.assertEqual(nand_gate(True, False), True)
+        self.assertEqual(nand_gate(0, 1), 1)
+        self.assertEqual(nand_gate(1, 0), 1)
 
     def test_nand_gate_both_true(self):
-        self.assertEqual(nand_gate(True, True), False)
+        self.assertEqual(nand_gate(1, 1), 0)
+
 
 class TestAndGate(unittest.TestCase):
-  def test_both_inputs_0(self):
-    result = and_gate(0, 0)
-    self.assertEqual(result, 0)
+    def test_both_inputs_0(self):
+        result = and_gate(0, 0)
+        self.assertEqual(result, 0)
 
-  def test_one_input_0(self):
-    result = and_gate(0, 1)
-    self.assertEqual(result, 0)
+    def test_one_input_0(self):
+        result = and_gate(0, 1)
+        self.assertEqual(result, 0)
 
-    result = and_gate(1, 0)
-    self.assertEqual(result, 0)
+        result = and_gate(1, 0)
+        self.assertEqual(result, 0)
 
-  def test_both_inputs_1(self):
-    result = and_gate(1, 1)
-    self.assertEqual(result, 1)
+    def test_both_inputs_1(self):
+        result = and_gate(1, 1)
+        self.assertEqual(result, 1)
+
 
 class TestNotGate(unittest.TestCase):
-  def test_input_0(self):
-    result = not_gate(0)
-    self.assertEqual(result, 1)
+    def test_input_0(self):
+        result = not_gate(0)
+        self.assertEqual(result, 1)
 
-  def test_input_1(self):
-    result = not_gate(1)
-    self.assertEqual(result, 0)
+    def test_input_1(self):
+        result = not_gate(1)
+        self.assertEqual(result, 0)
+
 
 class TestOrGate(unittest.TestCase):
-  def test_both_inputs_0(self):
-    result = or_gate(0, 0)
-    self.assertEqual(result, 0)
+    def test_both_inputs_0(self):
+        result = or_gate(0, 0)
+        self.assertEqual(result, 0)
 
-  def test_one_input_0(self):
-    result = or_gate(1, 0)
-    self.assertEqual(result, 1)
+    def test_one_input_0(self):
+        result = or_gate(1, 0)
+        self.assertEqual(result, 1)
 
-    result = or_gate(0, 1)
-    self.assertEqual(result, 1)
+        result = or_gate(0, 1)
+        self.assertEqual(result, 1)
 
-  def test_both_inputs_1(self):
-    result = or_gate(1, 1)
-    self.assertEqual(result, 1)
+    def test_both_inputs_1(self):
+        result = or_gate(1, 1)
+        self.assertEqual(result, 1)
 
-if __name__ == '__main__':
-  unittest.main()
+
+if __name__ == "__main__":
+    unittest.main()
