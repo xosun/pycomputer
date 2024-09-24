8468aa999274bf56e272c34d613a20234b810b3b
xosun <hello@daniellong.me>
2024-09-24 14:26:08 -0500

    Implement And gate with Nand gate

--- logic_gates.py
+++ logic_gates.py
@@ -1,2 +1,6 @@
 def nand_gate(a, b):
     return not (a and b)
+
+def and_gate(a, b):
+    ab_nand = nand_gate(a, b)
+    return nand_gate(ab_nand, ab_nand)
--- test_logic_gates.py
+++ test_logic_gates.py
@@ -1,5 +1,5 @@
 import unittest
-from logic_gates import nand_gate
+from logic_gates import nand_gate, and_gate
 
 class TestNandGate(unittest.TestCase):
     def test_nand_gate_all_false(self):
@@ -11,6 +11,22 @@ class TestNandGate(unittest.TestCase):
 
     def test_nand_gate_both_true(self):
         self.assertEqual(nand_gate(True, True), False)
+
+class TestAndGate(unittest.TestCase):
+  def test_both_inputs_0(self):
+    result = and_gate(0, 0)
+    self.assertEqual(result, 0)
+
+  def test_one_input_0(self):
+    result = and_gate(0, 1)
+    self.assertEqual(result, 0)
+
+    result = and_gate(1, 0)
+    self.assertEqual(result, 0)
+
+  def test_both_inputs_1(self):
+    result = and_gate(1, 1)
+    self.assertEqual(result, 1)
 
 if __name__ == '__main__':
     unittest.main()
