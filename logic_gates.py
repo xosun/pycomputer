bc0a88b45032172ae5ecf73472ffa990e9c3a5bd
xosun <hello@daniellong.me>
2024-09-26 09:52:36 -0500

    Add type hinting

--- logic_gates.py
+++ logic_gates.py
@@ -1,37 +1,37 @@
-def nand_gate(a, b):
+def nand_gate(a: int, b: int) -> int:
     return 1 if a == 0 or b == 0 else 0
 
 
-def and_gate(a, b):
+def and_gate(a: int, b: int) -> int:
     ab_nand = nand_gate(a, b)
     return nand_gate(ab_nand, ab_nand)
 
 
-def not_gate(a):
+def not_gate(a: int) -> int:
     return nand_gate(a, a)
 
 
-def or_gate(a, b):
+def or_gate(a: int, b: int) -> int:
     aa_nand = nand_gate(a, a)
     bb_nand = nand_gate(b, b)
     return nand_gate(aa_nand, bb_nand)
 
 
-def xor_gate(a, b):
+def xor_gate(a: int, b: int) -> int:
     ab_nand = nand_gate(a, b)
     a_and_ab_nand = nand_gate(a, ab_nand)
     b_and_ab_nand = nand_gate(b, ab_nand)
     return nand_gate(a_and_ab_nand, b_and_ab_nand)
 
 
-def mux_circuit(a, b, sel):
+def mux_circuit(a: int, b: int, sel: int) -> int:
     c = nand_gate(a, sel)
     d = nand_gate(sel, sel)
     e = nand_gate(b, d)
     return nand_gate(c, e)
 
 
-def dmux_circuit(sel, data):
+def dmux_circuit(sel: int, data: int) -> tuple[int, int]:
     not_sel = nand_gate(sel, sel)
     not_sel_data = nand_gate(not_sel, data)
     sel_data = nand_gate(sel, data)
